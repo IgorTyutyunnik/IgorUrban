652fd9e2e76f2ea2a06e057c5a07ed7559bffdc6
IgorTyutyunnik <olshik_pegas@mail.ru>
2024-07-05 20:35:42 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ def employees_rewrite(key):
         for k in keys_list:
             file_data[i][k.upper()] = file_data[i].pop(k)
     key = key.upper()
+    keys_list = list(file_data[0].keys())
     try:
-        if isinstance(file_data[1][key], int):
-            file_data_sorted = sorted(file_data, key=lambda user: user[key], reverse=True)
-        else:
-            file_data_sorted = sorted(file_data, key=lambda user: user[key])
-    except KeyError:
-        return print('Bad key for sorting')
-
-    with open("employees_SALARY_sorted.json", "w") as json_file:
+        j = 0
+        for p in keys_list:
+            if key == p:
+                j += 1
+                if isinstance(file_data[1][key], int):
+                    file_data_sorted = sorted(file_data, key=lambda user: user[key], reverse=True)
+                else:
+                    file_data_sorted = sorted(file_data, key=lambda user: user[key])
+        if j == 0:
+            raise ValueError
+    except Exception as error:
+        return print(error.__class__.__name__,"('Bad key for sorting')")
+    with open(f"employees_{key}_sorted.json", "w") as json_file:
         json.dump(file_data_sorted, json_file, indent=4)
 
-employees_rewrite('SALARY')
+employees_rewrite('departmen')
 
 
